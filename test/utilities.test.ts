8051e136f126ecc3e1cd750ea296bbbde2c89580
voorjaar <felitas@zoho.com>
2021-02-21 18:28:29 +0800

    fix test

--- test/utilities.test.ts
+++ test/utilities.test.ts
@@ -1,4 +1,4 @@
-import { readFileSync } from 'fs';
+import { readFileSync, writeFileSync } from 'fs';
 import { preprocess } from '../src/index';
 
 describe("comment style test", () => {
@@ -8,27 +8,15 @@ describe("comment style test", () => {
     result = await (await preprocess().markup({ content, filename: 'test.svelte'})).code;
   });
   it ("should generate utilities", () => {
+    writeFileSync('test.css', result);
     expect(result).toEqual(
 String.raw`<div   class="min-h-screen bg-gray-100 py-6 justify-center aspect-w-9 aspect-h-16 flex flex-col sm:py-12"></div>
 
 <style>
 ${readFileSync('./test/assets/preflight.css').toString()}
-:global(.min-h-screen) {
-  min-height: 100vh;
-}
 :global(.bg-gray-100) {
   --tw-bg-opacity: 1;
   background-color: rgba(243, 244, 246, var(--tw-bg-opacity));
-}
-:global(.py-6) {
-  padding-top: 1.5rem;
-  padding-bottom: 1.5rem;
-}
-:global(.justify-center) {
-  -webkit-box-pack: center;
-  -ms-flex-pack: center;
-  -webkit-justify-content: center;
-  justify-content: center;
 }
 :global(.flex) {
   display: -webkit-box;
@@ -42,6 +30,19 @@ ${readFileSync('./test/assets/preflight.css').toString()}
   -ms-flex-direction: column;
   -webkit-flex-direction: column;
   flex-direction: column;
+}
+:global(.min-h-screen) {
+  min-height: 100vh;
+}
+:global(.py-6) {
+  padding-top: 1.5rem;
+  padding-bottom: 1.5rem;
+}
+:global(.justify-center) {
+  -webkit-box-pack: center;
+  -ms-flex-pack: center;
+  -webkit-justify-content: center;
+  justify-content: center;
 }
 @media (min-width: 640px) {
   :global(.sm\:py-12) {
