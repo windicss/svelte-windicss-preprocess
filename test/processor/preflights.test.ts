9192534a102fed9fe08eca0c986f4f8f3ab567fd
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-03-03 07:29:26 +0100

    test: added test
    test if cache is ignored

--- test/processor/preflights.test.ts
+++ test/processor/preflights.test.ts
@@ -2,11 +2,19 @@ import { preprocess } from '../../src/index';
 import { testConfig } from '../helpers/utils';
 
 describe('Preflight test suite', () => {
-  let result: string;
+  let resultA: string;
+  let resultB: string;
+  let resultC: string;
   let content = '<p>Hello World!</p>';
+  let contentB = '<div>I am a supertest</div>';
   it('should generate preflights globally', async () => {
-    result = '';
-    result = (await preprocess({ ...testConfig }).markup({ content, filename: 'test.svelte' })).code;
-    expect(result).toMatchSnapshot('globalPreflight');
+    resultA = '';
+    resultA = (await preprocess({ ...testConfig }).markup({ content, filename: 'a.svelte' })).code;
+    expect(resultA).toMatchSnapshot('globalPreflight');
+    resultB = '';
+    resultB = (await preprocess({ ...testConfig }).markup({ content, filename: 'b.svelte' })).code;
+    resultC = '';
+    resultC = (await preprocess({ ...testConfig }).markup({ content, filename: 'a.svelte' })).code;
+    expect(resultC).toEqual(resultA);
   });
 });
