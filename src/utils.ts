2a56e6dd296e145c4967236b5b994b5441c52e60
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-03-11 19:59:13 +0100

    fix: delete outdated logging

--- src/utils.ts
+++ src/utils.ts
@@ -92,16 +92,6 @@ export function logging(options: Options) {
       options?.prefix ? chalk.gray(options.prefix) : chalk.yellowBold('not set')
     }\n`
   );
-  process.stdout.write(
-    `${chalk.blueBold('│')}    ${chalk.blackBold('•')} global preflights: ${
-      options?.globalPreflight == true ? chalk.gray('enabled') : chalk.gray('disabled')
-    }\n`
-  );
-  process.stdout.write(
-    `${chalk.blueBold('│')}    ${chalk.blackBold('•')} global utilities: ${
-      options?.globalUtility == true ? chalk.gray('enabled') : chalk.gray('disabled')
-    }\n`
-  );
 
   process.stdout.write(`${chalk.blueBold('│')}\n`);
   process.stdout.write(`${chalk.blueBold('└──────────')}\n`);
