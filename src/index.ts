334fa4e37adeb5dab0a8acb47648622e974918d7
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-03-26 11:10:42 +0100

    fix: inline expression parser

--- src/index.ts
+++ src/index.ts
@@ -165,10 +165,9 @@ function _preprocess(content: string, filename: string) {
         }
         // Match isolated inline expressions
         let INLINE_EXPRESSION = FINAL_TEXT_MATCHES[3].toString().match(/("|'|\s)[\{].*?[\}]/gi);
-
         // Extract classes from inline expressions
         if (INLINE_EXPRESSION) {
-          extractedClasses = FINAL_TEXT_MATCHES[3].replace(/'|\?|:|{(?=[^}]+?\?).+?\?|(?<=:[^{]+?)}/gi, '');
+          extractedClasses = FINAL_TEXT_MATCHES[3].replace(/{(?=[^{]+? [\w|']+})|(?<={\w+ [^{]+?)}|(?<={\w+ [^{]*?)['|:|\?]/gi, '');
         }
 
         if (OPTIONS.compile) {
