e72e74e4e0b950273a4b93b7461e72e2fd1496ec
Rafik El Hadi Houari <rehhouari@gmail.com>
2021-03-28 15:30:47 +0100

    Set image height to full on medium screen
    Image should take full height but it was not

--- site/src/App.svelte
+++ site/src/App.svelte
@@ -23,7 +23,7 @@
 <div class="max-w-md mx-auto bg-white rounded-xl shadow-md overflow-hidden md:max-w-2xl">
   <div class="md:flex">
     <div class="md:flex-shrink-0">
-      <img class="h-48 w-full object-cover md:w-48" src="https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80" alt="Man looking at item at a store">
+      <img class="h-48 md:h-full w-full object-cover md:w-48" src="https://images.unsplash.com/photo-1515711660811-48832a4c6f69?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80" alt="Man looking at item at a store">
     </div>
     <div class="p-8">
       <div class="uppercase tracking-wide text-sm text-indigo-500 font-semibold">Case study</div>
