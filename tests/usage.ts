dc5e06bd1e9c75db0acad8a46959bc0ad4d8f5f4
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-04-13 18:20:36 +0200

    test(magician): usage test (fail)

---
+++ tests/usage.ts
@@ -0,0 +1,161 @@
+import { readFileSync } from "fs";
+import { preprocess } from "svelte/compiler";
+import { test } from "uvu";
+import { fixture } from 'uvu/assert';
+
+
+test.before.each(() => {
+  delete require.cache[require.resolve("../dist/index")]
+});
+
+test("base", async () => {
+  let input = '<div class="bg-red-500">Hello World!</div>'
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, expected)
+})
+
+test("template in expression", async () => {
+  let input = '<div class={`bg-red-50`}>Hello World!</div>'
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test("windi in expression", async () => {
+  let input = '<div class={windi`bg-red-100`}>Hello World!</div>'
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test("wind in expression, with dynamic class", async () => {
+  let input = '<div class={windi`bg-red-${shade}`}>Hello World!</div>'
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test("expression", async () => {
+  let input = '<div class={dynamicClass}>Hello World!</div>'
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test("windi in expression with inline if", async () => {
+  let input = "<div class={windi`${toggle ? 'bg-blue-500' : 'bg-red-500'}`}>Hello World!</div>"
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test("inline expression with inline if and windi", async () => {
+  let input = "<div class={toggle ? windi`bg-yellow-500` : windi`bg-blue-500`}>Hello World!</div>"
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test("class directive", async () => {
+  let input = "<div class:bg-red-800={toggle} class:bg-blue-800={toggle}>Hello World!</div>"
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test("inline expression with inline if", async () => {
+  let input = "<div class={toggle ? 'text-white bg-blue-500' : 'bg-red-500'}>Hello World!</div>"
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test("advanced", async () => {
+  let input = `<div class="text-gray-100 test {blue ? 'test' : 'why'} language-{size} {className}">Hello World!</div>`
+
+  let expected = readFileSync("tests/assets/base.txt", "utf-8")
+
+  const { code } = await preprocess(input, require("../dist/index").preprocess({
+    silent: true,
+    debug: true
+  }), {
+    filename: 'base.svelte'
+  });
+  let actual = code
+  fixture(actual, "expected")
+})
+
+test.run()
