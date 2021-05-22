6c207986c43da7fe11f18b583fe44fbb3581837a
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-05-22 11:34:40 +0200

    test: use src instead of dist

--- tests/usage.ts
+++ tests/usage.ts
@@ -6,14 +6,14 @@ import { fixture } from 'uvu/assert'
 const classTest = suite('class attribute')
 
 classTest.before.each(() => {
-  delete require.cache[require.resolve('../dist/index')]
+  delete require.cache[require.resolve('../src/index')]
 })
 
 classTest('class_case-01', async () => {
   const input = readFileSync('tests/assets/input/classAttribute/case-01.svelte', 'utf-8')
   const expected = readFileSync('tests/assets/expected/classAttribute/case-01.svelte', 'utf-8')
   // eslint-disable-next-line
-  const { code } = await preprocess(input, require('../dist/index').windi({
+  const { code } = await preprocess(input, require('../src/index').windi({
     silent: true
   }), {
     filename: 'case-01.svelte'
@@ -26,7 +26,7 @@ classTest('class_case-02', async () => {
   const input = readFileSync('tests/assets/input/classAttribute/case-02.svelte', 'utf-8')
   const expected = readFileSync('tests/assets/expected/classAttribute/case-02.svelte', 'utf-8')
   // eslint-disable-next-line
-  const { code } = await preprocess(input, require('../dist/index').windi({
+  const { code } = await preprocess(input, require('../src/index').windi({
     silent: true
   }), {
     filename: 'case-02.svelte'
@@ -39,7 +39,7 @@ classTest('class_case-03', async () => {
   const input = readFileSync('tests/assets/input/classAttribute/case-03.svelte', 'utf-8')
   const expected = readFileSync('tests/assets/expected/classAttribute/case-03.svelte', 'utf-8')
   // eslint-disable-next-line
-  const { code } = await preprocess(input, require('../dist/index').windi({
+  const { code } = await preprocess(input, require('../src/index').windi({
     silent: true
   }), {
     filename: 'case-03.svelte'
@@ -52,7 +52,7 @@ classTest('class_case-04', async () => {
   const input = readFileSync('tests/assets/input/classAttribute/case-04.svelte', 'utf-8')
   const expected = readFileSync('tests/assets/expected/classAttribute/case-04.svelte', 'utf-8')
   // eslint-disable-next-line
-  const { code } = await preprocess(input, require('../dist/index').windi({
+  const { code } = await preprocess(input, require('../src/index').windi({
     silent: true
   }), {
     filename: 'case-04.svelte'
@@ -65,7 +65,7 @@ classTest('class_case-05', async () => {
   const input = readFileSync('tests/assets/input/classAttribute/case-05.svelte', 'utf-8')
   const expected = readFileSync('tests/assets/expected/classAttribute/case-05.svelte', 'utf-8')
   // eslint-disable-next-line
-  const { code } = await preprocess(input, require('../dist/index').windi({
+  const { code } = await preprocess(input, require('../src/index').windi({
     silent: true
   }), {
     filename: 'case-05.svelte'
