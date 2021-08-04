05ec4e9eff857d1532b6b689310710c03f343ba1
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-08-04 22:24:44 +0200

    refactor: :recycle: change to full file parser

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,23 @@
 import { useConfig, useDebugger } from '@nbhr/utils'
 import { readFileSync } from 'fs'
-import { createRequire } from 'module'
 import type { PreprocessorGroup } from 'svelte/types/compiler/preprocess'
 import { Processor } from 'windicss/lib'
 import type { FullConfig } from 'windicss/types/interfaces'
 import { CSSParser } from 'windicss/utils/parser'
 import { StyleSheet } from 'windicss/utils/style'
-import { combineStyleList, globalStyleSheet, Magician } from './utils'
+import { globalStyleSheet, Magician } from './utils'
 
 export interface Options {
-  silent?: boolean,
-  mode?: 'development' | 'production',
-  configPath?: string,
-  disableFormat?: boolean,
+  silent?: boolean
+  mode?: 'development' | 'production'
+  configPath?: string
+  disableFormat?: boolean
   devTools?: {
-    enabled: boolean,
-    completions?: boolean,
-  },
+    enabled: boolean
+    completions?: boolean
+  }
   //
-  safeList?: string,
+  safeList?: string
   preflights?: boolean
   // bundle?: string;
   // debug?: boolean;
@@ -33,10 +32,10 @@ let OPTIONS: Options = {
   // configPath
   disableFormat: false,
   devTools: {
-    enabled: false
+    enabled: false,
   },
   safeList: undefined,
-  preflights: true
+  preflights: true,
   // bundle: undefined,
   // compile: false,
   // prefix: 'windi-',
@@ -46,135 +45,90 @@ let OPTIONS: Options = {
 
 let DEV = false
 let PROCESSOR: Processor
-// let VARIANTS: string[] = [];
-// let BUNDLEFILE: string;
-// let IGNORED_CLASSES: string[] = [];
-// let STYLESHEETS: StyleSheet[] = [];
-// let CONDITIONS: StyleSheet[] = [];
-// let SAFELIST: StyleSheet[] = [];
-// let FILES: (string | undefined)[] = [];
-// let BUNDLES: { [key: string]: StyleSheet } = {};
-// let IS_MAIN: boolean = true;
-let isInit = false
+
 let windiConfig: FullConfig
 let CSS_SOURCE = ''
-let CSS_STYLESHEETS: Map<string,StyleSheet> = new Map()
-
-const REGEXP = {
-  matchStyle: /<style[^>]*?(\/|(>([\s\S]*?)<\/style))>/,
-  matchScript: /<script[^>]*?(\/|(>([\s\S]*?)<\/script))>/,
-  matchClasses: /('[\s\S]+?')|("[\s\S]+?")|(`[\s\S]+?`)/g,
-}
-const table: any = {}
-const size: any = 0
-const file: any = 0
+const CSS_STYLESHEETS: Map<string, { lastmodified: Date; code: StyleSheet }> = new Map()
 
 function _preprocess(content: string, filename: string) {
-
-  // // TODO: add magician logic
-  const run = 'new'
-  if (run == 'new') {
-    let mag = new Magician(PROCESSOR, content, filename, windiConfig)
-    mag = mag.clean()
-
-    if (!(OPTIONS?.disableFormat)) mag = mag.format()
-    mag = mag.setInject()
-    mag = mag
-      .each(line => {
-        return line
-          .processWindiExpression()
-          .processDirectiveClass()
-          .processAttributify()
-          .processClassAttribute()
-          .compute()
-      })
-
-    mag = mag
-      .compute()
-
-    // console.log(mag.getStats())
-
-    CSS_STYLESHEETS.set(filename, mag.getComputed())
-
-    return mag
-      .getCode()
-  } else {
-    // const COMPILED_CLASSES = PROCESSOR.compile(extractedClasses, OPTIONS.prefix, false);
-    // IGNORED_CLASSES = [...IGNORED_CLASSES, ...COMPILED_CLASSES.ignored];
-    // STYLESHEETS.push(COMPILED_CLASSES.styleSheet);
-    // let replacementValue = COMPILED_CLASSES.className
-    //   ? [COMPILED_CLASSES.className, ...COMPILED_CLASSES.ignored].join(' ')
-    //   : COMPILED_CLASSES.ignored.join(' ');
-
-    // lines[i] = lines[i].replace(new RegExp(TEXT_REGEX_MATCHER, 'i'), `$1${replacementValue}$4`);
-    return ''
-  }
+  let mag = new Magician(PROCESSOR, content, filename, windiConfig)
+  mag = mag.prepare()
+  mag = mag.setInject()
+  // mag = mag.each(line => {
+  //   return line.processWindiExpression().processDirectiveClass().processAttributify().processClassAttribute().compute()
+  // })
+  mag = mag.processClassAttribute().processDirectiveClass().processWindiExpression()
+  mag = mag.processAttributify()
+  mag = mag.compute()
+  CSS_STYLESHEETS.set(filename, { code: mag.getComputedStyleSheet(), lastmodified: new Date() })
+  return mag.getCode()
 }
 
 // Svelte evaluates preprocessors by running all markup preprocessors first,
 // then script and finally styles.
 // Some preprocesses may not work if other preprocessors haven't been run.
+// Currently that means we need to run the preprocessors in the right order, using svelte-sequential-preprocessor
+// TODO: move all logic back into markup
 export function windi(options: typeof OPTIONS = {}): PreprocessorGroup {
   PROCESSOR = new Processor()
   OPTIONS = { ...OPTIONS, ...options }
   DEV = false
 
-  if (process.env.NODE_ENV === 'production')  DEV = false
+  if (process.env.NODE_ENV === 'production') DEV = false
   if (process.env.NODE_ENV === 'development') DEV = true
   if (OPTIONS.mode === 'production') DEV = false
   if (OPTIONS.mode === 'development') DEV = true
 
   return {
     markup: ({ content, filename }) => {
-      return new Promise((resolve) => {
+      return new Promise(resolve => {
         if (windiConfig != undefined) {
           resolve({
-            code: _preprocess(content, filename)
+            code: _preprocess(content, filename),
           })
         } else {
           if (OPTIONS.configPath) {
             useDebugger.createLog('Trying to load windi configuration from ' + OPTIONS.configPath)
-            useConfig.load<FullConfig>(OPTIONS.configPath).then(config => {
-              if (config.preflight === false) OPTIONS.preflights = false
-              if (config.safelist && typeof config.safelist == 'string') {
-                OPTIONS.safeList = config.safelist
-              } else if (config.safelist) {
-                const tmpSafelist = config.safelist as (string | string[])[]
-                OPTIONS.safeList = [...new Set(tmpSafelist.flat(Infinity))].join(' ')
-              }
-              useDebugger.createLog('Configuration loaded successfully')
-              PROCESSOR.loadConfig(config)
-              windiConfig = config
-              // isInit = true
-            }).catch((e) => {
-              useDebugger.createLog('Unknown Error loading the config')
-              console.error(e)
-            }).finally(() => {
-              resolve({
-                code: _preprocess(content, filename)
+            useConfig
+              .load<FullConfig>(OPTIONS.configPath)
+              .then(config => {
+                if (config.preflight === false) OPTIONS.preflights = false
+                if (config.safelist && typeof config.safelist == 'string') {
+                  OPTIONS.safeList = config.safelist
+                } else if (config.safelist) {
+                  const tmpSafelist = config.safelist as (string | string[])[]
+                  OPTIONS.safeList = [...new Set(tmpSafelist.flat(Infinity))].join(' ')
+                }
+                PROCESSOR.loadConfig(config)
+                useDebugger.createLog('Configuration loaded successfully')
+                windiConfig = config
               })
-            })
+              .catch(e => {
+                useDebugger.createLog('Unknown Error while loading the config')
+                console.error(e)
+              })
+              .finally(() => {
+                resolve({
+                  code: _preprocess(content, filename),
+                })
+              })
           } else {
             PROCESSOR.loadConfig()
-            // isInit = true
             resolve({
-              code: _preprocess(content, filename)
+              code: _preprocess(content, filename),
             })
           }
         }
       })
     },
-    script: ({ content, attributes, markup }) => {
-      return new Promise(async (resolve) => {
-
+    script: ({ content, attributes }) => {
+      return new Promise(resolve => {
         if (DEV === true && OPTIONS.devTools && OPTIONS.devTools.enabled !== false && attributes['windi:devtools']) {
           const path = require.resolve('windicss-runtime-dom')
-          // const r = createRequire(import.meta.url)
-          // const path = r.resolve('windicss-runtime-dom')
           let runtimeConfig: FullConfig
           if (windiConfig !== undefined) {
             runtimeConfig = {
-              theme: windiConfig.theme
+              theme: windiConfig.theme,
             }
           } else {
             runtimeConfig = {}
@@ -197,20 +151,18 @@ export function windi(options: typeof OPTIONS = {}): PreprocessorGroup {
                 document.head.append(script);
               }
             `
-
           resolve({
-            code: injectScript + '\n' + content
+            code: injectScript + '\n' + content,
+          })
+        } else {
+          resolve({
+            code: content,
           })
         }
-
-        resolve({
-          code: content
-        })
-
       })
     },
-    style: ({ content, attributes, markup, filename }) => {
-      return new Promise((resolve) => {
+    style: ({ content, attributes, filename }) => {
+      return new Promise(resolve => {
         let PREFLIGHTS_STYLE = ''
         let SAFELIST_STYLE = ''
         let CSS_STYLE = ''
@@ -243,40 +195,42 @@ export function windi(options: typeof OPTIONS = {}): PreprocessorGroup {
         } else if (CSS_SOURCE) {
           const tmpCSS = CSS_SOURCE
           // global styles
-          const globalMatches = [...tmpCSS.matchAll(/:global\((?<selector>.*)\).*{(?<css>[^}]*)}/gmi) || []].map(el => {
-            return `${el.groups?.selector} {${el.groups?.css}}`
-          }).join('\n')
+          const globalMatches = [...(tmpCSS.matchAll(/:global\((?<selector>.*)\).*{(?<css>[^}]*)}/gim) || [])]
+            .map(el => {
+              return `${el.groups?.selector} {${el.groups?.css}}`
+            })
+            .join('\n')
           const globalCSS = new CSSParser(globalMatches, PROCESSOR).parse()
           const buildGlobalCSS = globalStyleSheet(globalCSS).build()
-          if(buildGlobalCSS.length > 0) CSS_STYLE += buildGlobalCSS + '\n'
+          if (buildGlobalCSS.length > 0) CSS_STYLE += buildGlobalCSS + '\n'
           // local styles
-          const scopedMatches = tmpCSS.replace(/:global\([^}]*}/gmi, '')
+          const scopedMatches = tmpCSS.replace(/:global\([^}]*}/gim, '')
           CSS = new CSSParser(scopedMatches, PROCESSOR).parse()
           const buildLocalCSS = CSS.build()
-          if(buildLocalCSS.length > 0) CSS_STYLE += buildLocalCSS
+          if (buildLocalCSS.length > 0) CSS_STYLE += buildLocalCSS
         }
 
         // MARK: WINDI CSS
         if (filename && CSS_STYLESHEETS.has(filename) && attributes['windi:global']) {
           const FILESHEET = CSS_STYLESHEETS.get(filename)!
-          INLINE_STYLE = globalStyleSheet(FILESHEET).build()
+          INLINE_STYLE = globalStyleSheet(FILESHEET['code']).build()
         } else if (filename && CSS_STYLESHEETS.has(filename)) {
           const FILESHEET = CSS_STYLESHEETS.get(filename)!
-          INLINE_STYLE = FILESHEET.build()
+          INLINE_STYLE = FILESHEET['code'].build()
         }
 
         // MARK: COMBINE
         let newStyleCode = '\n'
-        if(PREFLIGHTS_STYLE.length > 0) newStyleCode += PREFLIGHTS_STYLE + '\n'
-        if(SAFELIST_STYLE.length > 0) newStyleCode += SAFELIST_STYLE + '\n'
-        if(CSS_STYLE.length > 0) newStyleCode += CSS_STYLE + '\n'
-        if(INLINE_STYLE.length > 0) newStyleCode += INLINE_STYLE + '\n'
+        if (PREFLIGHTS_STYLE.length > 0) newStyleCode += PREFLIGHTS_STYLE + '\n'
+        if (SAFELIST_STYLE.length > 0) newStyleCode += SAFELIST_STYLE + '\n'
+        if (CSS_STYLE.length > 0) newStyleCode += CSS_STYLE + '\n'
+        if (INLINE_STYLE.length > 0) newStyleCode += INLINE_STYLE + '\n'
 
         resolve({
-          code: newStyleCode
+          code: newStyleCode,
         })
       })
-    }
+    },
   }
 }
 
--- src/utils.ts
+++ src/utils.ts
@@ -1,10 +1,6 @@
-import { readFileSync } from 'fs'
-import { performance } from 'perf_hooks'
-import { format, RequiredOptions } from 'prettier'
 import Processor from 'windicss'
 import type { FullConfig } from 'windicss/types/interfaces'
 import { StyleSheet } from 'windicss/utils/style'
-import type { Options } from './index'
 
 export function combineStyleList(stylesheets: StyleSheet[]): StyleSheet {
   return stylesheets
@@ -25,107 +21,17 @@ export function globalStyleSheet(styleSheet: StyleSheet): StyleSheet {
   return styleSheet
 }
 
-class Step {
-  processor: Processor
-  content: string
-  filename: string
-  windiClassList: string[] = []
-  directiveClassList: string[] = []
-  attributifyClassList: Map<string, string[]> = new Map()
-  mainClassList: string[] = []
-  constructor(processor: Processor, content: string, filename: string) {
-    this.processor = processor
-    this.content = content
-    this.filename = filename
+interface Computed {
+  default: {
+    success: string[]
+    ignored: string[]
+    styleSheet: StyleSheet
   }
-
-  processWindiExpression() {
-
-    const tmpContent = this.content
-    const WINDI_MATCHES = [...tmpContent.matchAll(/windi`(.*?)`/gi)]
-    if (WINDI_MATCHES.length < 1) return this
-    for (const match of WINDI_MATCHES) {
-      const cleanedMatch = match[1]
-        .replace(/(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['":]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi, ' ')
-        .replace(/ {2,}/gi, ' ')
-        .replace(/["'`]/gi, '')
-      this.windiClassList = this.windiClassList.concat(cleanedMatch.split(' '))
-    }
-
-    this.content = tmpContent
-    return this
-
+  attributify: {
+    success: string[]
+    ignored: string[]
+    styleSheet: StyleSheet
   }
-
-  processDirectiveClass() {
-
-    const tmpContent = this.content
-    const DIRECTIVE_CLASS_MATCHES = [...tmpContent.matchAll(/\s(class):([^=]+)(=)/gi)]
-    if (DIRECTIVE_CLASS_MATCHES.length < 1) return this
-    for (const match of DIRECTIVE_CLASS_MATCHES) {
-      this.directiveClassList.push(match[2])
-    }
-
-    this.content = tmpContent
-    return this
-  }
-
-  processAttributify() {
-
-    // FIXME: #150 not bulletprof yet
-    const tmpContent = this.content
-    const ATTRIBUTIFY_CLASS_MATCHES = [...tmpContent.matchAll(/([\w+:_/-]+)=(['"])([^\2]*?)\2/gi)]
-    // TODO: allow prefix with ::
-    // extract key & value as class array
-    if (ATTRIBUTIFY_CLASS_MATCHES.length < 1) return this
-    for (const match of ATTRIBUTIFY_CLASS_MATCHES) {
-      if (match[1] == 'class') continue
-      const cleanedMatch = match[3]
-        .replace(/windi[`].+?[`]|(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['"]|(?<=([{][\w\s]+[^{]*?))\s[:]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi, ' ')
-        .replace(/ {2,}/gi, ' ')
-        .replace(/["'`]/gi, '')
-      this.attributifyClassList.set(match[1].toString(), cleanedMatch.split(' '))
-    }
-
-    this.content = tmpContent
-    return this
-  }
-
-  processClassAttribute() {
-
-    const tmpContent = this.content
-    const CLASS_MATCHES = [...tmpContent.matchAll(/(class)=(['"])([^\2]*?)\2/gi)]
-    if (CLASS_MATCHES.length < 1) return this
-    for (const match of CLASS_MATCHES) {
-      const cleanedMatch = match[3]
-        .replace(/windi[`].+?[`]|(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['"]|(?<=([{][\w\s]+[^{]*?))\s[:]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi, ' ')
-        .replace(/ {2,}/gi, ' ')
-        .replace(/["'`]/gi, '')
-      this.mainClassList = cleanedMatch.split(' ')
-    }
-
-    this.content = tmpContent
-    return this
-  }
-
-  compute(compile = false) {
-
-    return {
-      line: this.content,
-      expressions: this.windiClassList,
-      directives: this.directiveClassList,
-      attributifies: this.attributifyClassList,
-      classes: this.mainClassList
-    }
-  }
-}
-
-interface CustomPrettierOptions extends Partial<RequiredOptions> {
-  svelteSortOrder: string,
-  svelteStrictMode: boolean
-  svelteAllowShorthand: boolean,
-  svelteBracketNewLine: boolean
-  svelteIndentScriptAndStyle: boolean
 }
 
 export class Magician {
@@ -141,7 +47,8 @@ export class Magician {
   classes: string[] = []
   stylesheets: StyleSheet[] = []
   config: FullConfig = {}
-  stats: Map<Record<string, string>,Record<string, string|number>> = new Map()
+  stats: Map<Record<string, string>, Record<string, string | number>> = new Map()
+  computed: Computed | undefined = undefined
   computedStyleSheet: StyleSheet = new StyleSheet()
   css = ''
 
@@ -152,13 +59,13 @@ export class Magician {
     this.config = config
   }
 
-  getStats(): Map<Record<string, string>,Record<string, string|number>> {
+  getStats(): Map<Record<string, string>, Record<string, string | number>> {
     return this.stats
   }
 
-  clean(): this {
+  prepare(): this {
     let tmpContent = this.content
-    // FIXME: CodeQL CODE WARNING
+    // TODO: refactor so that preprocessor persists comments
     tmpContent = tmpContent.replace(/<!--[\s\S]*?-->/g, '')
     tmpContent = tmpContent.replace(/([!\w][\w:_/-]*?):\(([\w\s/-]*?)\)/gm, (_, groupOne: string, groupTwo: string) =>
       groupTwo
@@ -168,34 +75,6 @@ export class Magician {
     )
 
     this.content = tmpContent
-    return this
-  }
-
-  format(): this {
-    // TODO: better formatting.. no upstream fix of prettier-plugin expected soon
-    // https://github.com/sveltejs/prettier-plugin-svelte/issues/214
-
-    const start = performance.now()
-    let tmpContent = this.content
-
-    tmpContent = tmpContent.replace(/(?<=[<]{1}\w[^>]+)\n/gmi, ' ')
-
-    const options: CustomPrettierOptions = {
-      parser: 'svelte',
-      plugins: ['prettier-plugin-svelte'],
-      printWidth: 5000,
-      tabWidth: 2,
-      svelteSortOrder: 'options-scripts-markup-styles',
-      svelteStrictMode: true,
-      svelteAllowShorthand: false,
-      svelteBracketNewLine: false,
-      svelteIndentScriptAndStyle: false,
-    }
-    const formatedContent = format(tmpContent, options)
-
-    this.content = formatedContent
-    this.stats.set({file: this.filename, category: 'format'},{text: 'Format .svelte File by Prettier', time: (performance.now() - start).toFixed(2)})
-
     return this
   }
 
@@ -213,71 +92,120 @@ export class Magician {
     return this
   }
 
-  each(callbackfn: (line: Step) => {
-    line: string,
-    expressions: string[],
-    directives: string[],
-    attributifies: Map<string, string[]>,
-    classes: string[]
-  }): this {
-    let tmpContent = this.content
+  processClassAttribute(): this {
+    const tmpContent = this.content
+    const CLASS_MATCHES = [...tmpContent.matchAll(/(class)=(['"])(?<classes>[^\2]*?)\2/gi)]
+    if (CLASS_MATCHES.length < 1) return this
+    for (const match of CLASS_MATCHES) {
+      const cleanedMatch = match[3]
+        .replace(
+          /windi[`].+?[`]|(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['"]|(?<=([{][\w\s]+[^{]*?))\s[:]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi,
+          ' '
+        )
+        .replace(/\n/gi, ' ')
+        .replace(/ {2,}/gi, ' ')
+        .replace(/["'`]/gi, '')
+      this.classes = this.classes.concat(
+        cleanedMatch.split(' ').filter(c => {
+          return c.length > 0
+        })
+      )
+    }
+    //console.log(this.classes)
 
-    this.lines = tmpContent.split('\n')
-    this.lines.forEach(el => {
-      // let result
-      const { line, expressions, directives, attributifies, classes } = callbackfn(new Step(this.processor, el, this.filename))
-      el = line
-      this.expressions = this.expressions.concat(expressions)
-      this.directives = this.directives.concat(directives)
-      attributifies.forEach((v, k) => {
-        if (this.attributifies.has(k)) {
-          const oldValue = this.attributifies.get(k)
-          if (oldValue) {
-            this.attributifies.set(k, oldValue.concat(v))
-          }
-        } else {
-          this.attributifies.set(k, v)
-        }
-      })
-      this.classes = this.classes.concat(classes)
-      // this.stylesheets = this.stylesheets.concat(sheets)
-    })
-    this.attributifies.forEach((v, k) => {
-      const unique = new Set(v)
-      this.attributifies.set(k, Array.from(unique))
-    })
-    tmpContent = this.lines.join('\n')
     this.content = tmpContent
+    return this
+  }
 
+  processDirectiveClass(): this {
+    const tmpContent = this.content
+    const DIRECTIVE_CLASS_MATCHES = [...tmpContent.matchAll(/\s(class):(?<class>[^=]+)(=)/gi)]
+    if (DIRECTIVE_CLASS_MATCHES.length < 1) return this
+    for (const match of DIRECTIVE_CLASS_MATCHES) {
+      this.directives = this.directives.concat(match[2])
+      // this.directiveClassList.push(match[2])
+    }
+    //console.log(this.directives)
+    this.content = tmpContent
+    return this
+  }
+
+  processWindiExpression(): this {
+    const tmpContent = this.content
+    const WINDI_MATCHES = [...tmpContent.matchAll(/windi`(?<class>.*?)`/gi)]
+    if (WINDI_MATCHES.length < 1) return this
+    for (const match of WINDI_MATCHES) {
+      const cleanedMatch = match[1]
+        .replace(/(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['":]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi, ' ')
+        .replace(/ {2,}/gi, ' ')
+        .replace(/["'`]/gi, '')
+      this.expressions = this.expressions.concat(cleanedMatch.split(' '))
+      // this.windiClassList = this.windiClassList.concat(cleanedMatch.split(' '))
+    }
+    //console.log(this.expressions)
+    this.content = tmpContent
+    return this
+  }
+
+  processAttributify(): this {
+    // FIXME: #150 not bulletprof yet
+    const tmpContent = this.content
+    const ATTRIBUTIFY_CLASS_MATCHES = [...tmpContent.matchAll(/([\w+:_/-]+)=(['"])(?<classes>[^\2]*?)\2/gi)]
+    // TODO: allow prefix with ::
+    // extract key & value as class array
+    if (ATTRIBUTIFY_CLASS_MATCHES.length < 1) return this
+    for (const match of ATTRIBUTIFY_CLASS_MATCHES) {
+      if (match[1] == 'class') continue
+      if (match[1] == 'href') continue
+      if (match[1] == 'this') continue
+      if (match[1] == 'name') continue
+      if (match[1] == 'stroke') continue
+      if (match[1] == 'd') continue
+      if (match[1] == 'slot') continue
+      if (match[1] == 'viewBox') continue
+      if (match[1] == 'points') continue
+      if (match[1] == 'label') continue
+      if (match[1].startsWith('class:')) continue
+      const cleanedMatch = match[3]
+        .replace(
+          /windi[`].+?[`]|(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['"]|(?<=([{][\w\s]+[^{]*?))\s[:]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi,
+          ' '
+        )
+        .replace(/ {2,}/gi, ' ')
+        .replace(/["'`]/gi, '')
+      // this.attributifyClassList.set(match[1].toString(), cleanedMatch.split(' '))
+
+      if (this.attributifies.has(match[1].toString())) {
+        const oldValue = this.attributifies.get(match[1].toString())
+        if (oldValue) {
+          this.attributifies.set(match[1].toString(), oldValue.concat(cleanedMatch.split(' ')))
+        }
+      } else {
+        this.attributifies.set(match[1].toString(), cleanedMatch.split(' '))
+      }
+    }
+    //console.log(this.attributifies)
+
+    this.content = tmpContent
     return this
   }
 
   compute(): this {
     const tmpContent = this.content
 
-    const windiSet = new Set(this.expressions)
-    const INTERPRETED_WINDI = this.processor.interpret( Array.from(windiSet).join(' ')).styleSheet
+    const defaultSet = new Set(this.classes.concat(this.directives).concat(this.expressions))
+    const INTERPRETED_DEFAULT = this.processor.interpret(Array.from(defaultSet).join(' '))
 
+    this.attributifies.forEach((v, k) => {
+      const unique = new Set(v)
+      this.attributifies.set(k, Array.from(unique))
+    })
+    const INTERPRETED_ATTRIBUTIFY = this.processor.attributify(Object.fromEntries(this.attributifies))
 
-    const directiveSet = new Set(this.directives)
-    const INTERPRETED_DIRECTIVE = this.processor.interpret(Array.from(directiveSet).join(' ')).styleSheet
-    const start = performance.now()
-    const INTERPRETED_ATTRIBUTIFY = this.processor.attributify(Object.fromEntries(this.attributifies)).styleSheet
-    this.stats.set({file: this.filename, category: 'attributify'},{text: 'Handle Attributify by Windi CSS', time: (performance.now() - start).toFixed(2)})
-
-    const classSet = new Set(this.classes)
-    const start2 = performance.now()
-    const INTERPRETED_MAIN = this.processor.interpret(Array.from(classSet).join(' ')).styleSheet
-    this.stats.set({file: this.filename, category: 'classes'},{text: 'Handle usual Classes by Windi CSS', time: (performance.now() - start2).toFixed(2)})
-
-    // windiexpression
-    this.stylesheets.push(INTERPRETED_DIRECTIVE)
-    this.stylesheets.push(INTERPRETED_ATTRIBUTIFY)
-    this.stylesheets.push(INTERPRETED_WINDI)
-    this.stylesheets.push(INTERPRETED_MAIN)
-    const start3 = performance.now()
+    this.stylesheets.push(INTERPRETED_DEFAULT.styleSheet)
+    this.stylesheets.push(INTERPRETED_ATTRIBUTIFY.styleSheet)
+    this.computed = { default: INTERPRETED_DEFAULT, attributify: INTERPRETED_ATTRIBUTIFY }
     this.computedStyleSheet = combineStyleList(this.stylesheets).sort()
-    this.stats.set({file: this.filename, category: 'sort'},{text: 'Combine and sort Styles by Windi CSS', time: (performance.now() - start3).toFixed(2)})
 
     this.content = tmpContent
     return this
@@ -287,7 +215,10 @@ export class Magician {
     return this.content
   }
 
-  getComputed(): StyleSheet {
+  getComputed(): Computed | undefined {
+    return this.computed
+  }
+  getComputedStyleSheet(): StyleSheet {
     return this.computedStyleSheet
   }
 
