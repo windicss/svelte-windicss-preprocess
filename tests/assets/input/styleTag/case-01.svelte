2b0b7b3e0e7daaf005714c2a02d1947464d26c81
Alexander Niebuhr <45965090+alexanderniebuhr@users.noreply.github.com>
2021-08-09 13:43:52 +0200

    test: :white_check_mark: add test case #208

---
+++ tests/assets/input/styleTag/case-01.svelte
@@ -0,0 +1,24 @@
+<script>
+</script>
+
+<label for="demo">
+  <p>Test</p>
+</label>
+
+<style>
+  label > :global(*) {
+    pointer-events: none;
+    @apply bg-red-500;
+  }
+  :global(*) {
+    pointer-events: none;
+    @apply bg-green-500;
+  }
+  test {
+    pointer-events:none;
+    @apply bg-yellow-500;
+  }
+  :global(div) > div {
+	  color: red;
+  }
+</style>
