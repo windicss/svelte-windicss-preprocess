16f7efa6e0232a8ce59e50a757ae99c332b119c5
Alexander Niebuhr <tech@nbhr.io>
2021-10-26 10:38:23 +0200

    test: :white_check_mark: add multiline css selector tests

---
+++ tests/assets/expected/styleTag/case-02.svelte
@@ -0,0 +1,27 @@
+<script>
+  // your script goes here
+</script>
+
+<style windi:inject>
+input, textarea, select {
+  --tw-border-opacity: 1;
+  border-color: rgba(229, 231, 235, var(--tw-border-opacity));
+  border-style: solid;
+  border-width: 1px;
+  --tw-text-opacity: 1;
+  color: rgba(0, 0, 0, var(--tw-text-opacity));
+  border-radius: 0.25rem;
+  outline: 2px solid transparent;
+  outline-offset: 2px;
+  font-weight: 400;
+  font-size: 0.875rem;
+  line-height: 1.25rem;
+  padding: 0.125rem;
+  padding-left: 0.25rem;
+  --tw-bg-opacity: 1;
+  background-color: rgba(255, 255, 255, var(--tw-bg-opacity));
+  width: 100%;
+}
+
+</style>
+
---
+++ tests/assets/input/styleTag/case-02.svelte
@@ -0,0 +1,19 @@
+<script>
+  // your script goes here
+</script>
+
+<style>
+  input,
+  textarea,
+  select {
+    @apply border border-gray-200 text-black border-solid;
+    @apply focus:border-secundary hover:border-primary-light;
+    @apply rounded outline-none;
+    @apply text-sm p-0.5 pl-1 font-normal;
+    @apply placeholder-neutro-ligth;
+    @apply bg-white;
+    @apply w-full;
+  }
+</style>
+
+<!-- markup (zero or more items) goes here -->
