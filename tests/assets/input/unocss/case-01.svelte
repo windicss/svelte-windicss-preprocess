a4f1bd0c77a7bca574d7515e392cab96e42ce871
Alexander Niebuhr <tech@nbhr.io>
2021-10-31 15:36:11 +0100

    test: :white_check_mark: add unocss pure css icons test

---
+++ tests/assets/input/unocss/case-01.svelte
@@ -0,0 +1,11 @@
+<script>
+  // your script goes here
+</script>
+
+<style>
+  /* your styles go here */
+</style>
+
+<!-- markup (zero or more items) goes here -->
+<div class="i-mdi:close"></div>
+<div class="i-mdi:archive-arrow-down"></div>
