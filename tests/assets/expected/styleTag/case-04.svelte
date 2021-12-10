e79e05f5298342120827212fe164d82f3dd952e9
Alexander Niebuhr <tech@nbhr.io>
2021-12-10 14:53:18 +0100

    test: add test for case with empty style tags

---
+++ tests/assets/expected/styleTag/case-04.svelte
@@ -0,0 +1,13 @@
+<script>
+</script>
+
+<div class="bg-red-50">
+  <h1>Hello World</h1>
+</div>
+
+<style>
+.bg-red-50 {
+  --tw-bg-opacity: 1;
+  background-color: rgba(254, 242, 242, var(--tw-bg-opacity));
+}
+</style>
