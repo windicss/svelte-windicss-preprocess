0ecba21cdf37daf7093e93c703e560fcc7da8691
Alexander Niebuhr <tech@nbhr.io>
2022-01-17 10:12:28 +0100

    refactor: :recycle: update utility scan logic

--- src/utils.ts
+++ src/utils.ts
@@ -10,10 +10,9 @@ export interface SetObject {
 
 export class FileHandler {
   private content: string
-  classes: string[] = []
-  expressions: string[] = []
-  directives: string[] = []
+  utilities: string[] = []
   attributifies: Map<string, string[]> = new Map()
+  icons: string[] = []
 
   constructor(content: string) {
     console.log('CONSTRUCT')
@@ -54,190 +53,148 @@ export class FileHandler {
     return this
   }
 
-  processClassAttribute(): this {
-    const CLASS_MATCHES = [
-      ...this.content.matchAll(/class=(['"`])(?<classes>[^\1]+?)\1/gi),
-    ]
-    if (CLASS_MATCHES.length < 1) return this
-    for (const match of CLASS_MATCHES) {
-      const cleanMatch = match.groups?.classes
-        .replaceAll(/windi[`].+?[`]/gi, ' ') // windi`XYZ`
-        .replaceAll(/(?<![-])[$](?=[{])/gi, ' ') // if leading char is not -, and next char is {, then remove $
-        .replaceAll(/(?<=([{][\w\s]+[^{]*?))['"]/gi, ' ') // remove quotes in curly braces
-        .replaceAll(/(?<=([{][\w\s]+[^{]*?)\s)[:]/gi, ' ') // remove : in curly braces
-        .replaceAll(/([{][\w\s]+[^{]*?[?])/gi, ' ') // remove ? and condition in curly braces
-        .replaceAll(/[{}]/gi, ' ') // remove curly braces
-        .replaceAll(/\n/gi, ' ') // remove newline
-        .replaceAll(/ {2,}/gi, ' ') // remove multiple spaces
-        .replaceAll(/["'`]/gi, '') // remove quotes
-
-      this.classes = this.classes.concat(
-        (cleanMatch || '').split(' ').filter(c => {
-          return c.length > 0
-        })
-      )
-    }
-    return this
-  }
-
-  processClassAttributeWithCurly(): this {
-    const CLASS_MATCHES = [
-      ...this.content.matchAll(/class=([{])(?<classes>[^}]+?)}/gi),
-    ]
-
-    if (CLASS_MATCHES.length < 1) return this
-    for (const match of CLASS_MATCHES) {
-      const cleanMatch = match.groups?.classes
-        .replaceAll(/windi[`].+?[`]/gi, ' ') // windi`XYZ`
-        .replaceAll(/(?<![-])[$](?=[{])/gi, ' ') // if leading char is not -, and next char is {, then remove $
-        .replaceAll(/(?<=([{][\w\s]+[^{]*?))['"]/gi, ' ') // remove quotes in curly braces
-        .replaceAll(/(?<=([{][\w\s]+[^{]*?)\s)[:]/gi, ' ') // remove : in curly braces
-        .replaceAll(/([{][\w\s]+[^{]*?[?])/gi, ' ') // remove ? and condition in curly braces
-        .replaceAll(/[{}]/gi, ' ') // remove curly braces
-        .replaceAll(/\n/gi, ' ') // remove newline
-        .replaceAll(/ {2,}/gi, ' ') // remove multiple spaces
-        .replaceAll(/["'`]/gi, '') // remove quotes
-
-      this.classes = this.classes.concat(
-        (cleanMatch || '').split(' ').filter(c => {
-          return c.length > 0
-        })
-      )
-    }
-    return this
-  }
-
-  processDirectiveClass(): this {
-    const tmpContent = this.content
-    const DIRECTIVE_CLASS_MATCHES = [
-      ...tmpContent.matchAll(/\s(class):(?<class>[^=]+)(=)/gi),
-    ]
-    if (DIRECTIVE_CLASS_MATCHES.length < 1) return this
-    for (const match of DIRECTIVE_CLASS_MATCHES) {
-      this.directives = this.directives.concat(match[2])
-      // this.directiveClassList.push(match[2])
-    }
-    this.content = tmpContent
-    return this
-  }
-
-  processWindiExpression(): this {
-    const tmpContent = this.content
-    const WINDI_MATCHES = [...tmpContent.matchAll(/windi`(?<class>.*?)`/gi)]
-    if (WINDI_MATCHES.length < 1) return this
-    for (const match of WINDI_MATCHES) {
-      const cleanedMatch = match[1]
-        .replace(
-          /(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['":]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi,
-          ' '
-        )
-        .replace(/ {2,}/gi, ' ')
-        .replace(/["'`]/gi, '')
-      this.expressions = this.expressions.concat(cleanedMatch.split(' '))
-      // this.windiClassList = this.windiClassList.concat(cleanedMatch.split(' '))
-    }
-    this.content = tmpContent
-    return this
-  }
-
-  processAttributify(): this {
-    const ATTRIBUTIFY_CLASS_MATCHES = [
-      ...this.content.matchAll(/([\w+:_/-]+)=(['"`])(?<classes>[^\2]+?)\2/gi),
-    ]
-
-    if (ATTRIBUTIFY_CLASS_MATCHES.length < 1) return this
-    for (const match of ATTRIBUTIFY_CLASS_MATCHES) {
-      if (match[1] == 'class') continue
-      if (match[1] == 'href') continue
-      if (match[1] == 'for') continue
-      // if (match[1] == 'this') continue
-      // if (match[1] == 'name') continue
-      // if (match[1] == 'stroke') continue
-      // if (match[1] == 'd') continue
-      // if (match[1] == 'slot') continue
-      // if (match[1] == 'viewBox') continue
-      // if (match[1] == 'points') continue
-      // if (match[1] == 'label') continue
-      // if (match[1] == 'xmlns') continue
-      if (match[1].startsWith('class:')) continue
-      const cleanedMatch = match[3]
-        .replace(
-          /windi[`].+?[`]|(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['"]|(?<=([{][\w\s]+[^{]*?))\s[:]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi,
-          ' '
-        )
-        .replace(/ {2,}/gi, ' ')
-        .replace(/["'`]/gi, '')
-        .replace(/\n/gi, ' ')
-
-      if (this.attributifies.has(match[1].toString())) {
-        const oldValue = this.attributifies.get(match[1].toString())
-        if (oldValue) {
-          this.attributifies.set(
-            match[1].toString(),
-            oldValue.concat(cleanedMatch.split(' '))
-          )
-        }
-      } else {
-        this.attributifies.set(match[1].toString(), cleanedMatch.split(' '))
-      }
-    }
-    return this
+  private cleanUtility(dirtyClasses: string): string {
+    return dirtyClasses
+      .replaceAll(/windi[`].+?[`]/gi, ' ') // windi`XYZ`
+      .replaceAll(/(?<![-])[$](?=[{])/gi, ' ') // if leading char is not -, and next char is {, then remove $
+      .replaceAll(/(?<=([{][\w\s]+[^{]*?))['"]/gi, ' ') // remove quotes in curly braces
+      .replaceAll(/(?<=([{][\w\s]+[^{]*?)\s)[:]/gi, ' ') // remove : in curly braces
+      .replaceAll(/([{][\w\s]+[^{]*?[?])/gi, ' ') // remove ? and condition in curly braces
+      .replaceAll(/[{}]/gi, ' ') // remove curly braces
+      .replaceAll(/\n/gi, ' ') // remove newline
+      .replaceAll(/ {2,}/gi, ' ') // remove multiple spaces
+      .replaceAll(/["'`]/gi, '') // remove quotes
   }
 
   public scan(): this {
     console.log('SCAN')
 
-    this.processClassAttribute()
-    this.processClassAttributeWithCurly()
-    this.processDirectiveClass()
-    this.processWindiExpression()
-    this.processAttributify()
+    const CLASSATTRIBUTE_MATCHES = [
+      ...this.content.matchAll(/class=(['"`])(?<utilities>[^\1]+?)\1/gi),
+      ...this.content.matchAll(/class=(?<utilities>[{][^}]+?)}/gi),
+    ]
+
+    for (const match of CLASSATTRIBUTE_MATCHES) {
+      const cleanedMatch = this.cleanUtility(match.groups?.utilities || '')
+      this.utilities = this.utilities.concat(
+        cleanedMatch.split(' ').filter(c => {
+          if (c.startsWith('i-')) return false
+          return c.length > 0
+        })
+      )
+      this.icons = this.icons.concat(
+        cleanedMatch.split(' ').filter(c => {
+          if (!c.startsWith('i-')) return false
+          return c.length > 0
+        })
+      )
+    }
+
+    const DIRECTIVE_MATCHES = [
+      ...this.content.matchAll(/\s(class):(?<utility>[^=]+)(=)/gi),
+    ]
+    for (const match of DIRECTIVE_MATCHES) {
+      if (match.groups?.utility.startsWith('i-')) {
+        this.icons = this.icons.concat(match.groups?.utility)
+      } else if (match.groups?.utility) {
+        this.utilities = this.utilities.concat(match.groups?.utility)
+      }
+    }
+
+    console.log('utility', this.utilities)
+    console.log('icons', this.icons)
+
+    // this.processWindiExpression()
+    // this.processAttributify()
 
     return this
   }
 
-  public getStyles(): SetObject {
+  // processWindiExpression(): this {
+  //   const tmpContent = this.content
+  //   const WINDI_MATCHES = [...tmpContent.matchAll(/windi`(?<class>.*?)`/gi)]
+  //   if (WINDI_MATCHES.length < 1) return this
+  //   for (const match of WINDI_MATCHES) {
+  //     const cleanedMatch = match[1]
+  //       .replace(
+  //         /(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['":]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi,
+  //         ' '
+  //       )
+  //       .replace(/ {2,}/gi, ' ')
+  //       .replace(/["'`]/gi, '')
+  //     this.expressions = this.expressions.concat(cleanedMatch.split(' '))
+  //     // this.windiClassList = this.windiClassList.concat(cleanedMatch.split(' '))
+  //   }
+  //   this.content = tmpContent
+  //   return this
+  // }
+
+  // processAttributify(): this {
+  //   const ATTRIBUTIFY_CLASS_MATCHES = [
+  //     ...this.content.matchAll(/([\w+:_/-]+)=(['"`])(?<classes>[^\2]+?)\2/gi),
+  //   ]
+
+  //   if (ATTRIBUTIFY_CLASS_MATCHES.length < 1) return this
+  //   for (const match of ATTRIBUTIFY_CLASS_MATCHES) {
+  //     if (match[1] == 'class') continue
+  //     if (match[1] == 'href') continue
+  //     if (match[1] == 'for') continue
+  //     // if (match[1] == 'this') continue
+  //     // if (match[1] == 'name') continue
+  //     // if (match[1] == 'stroke') continue
+  //     // if (match[1] == 'd') continue
+  //     // if (match[1] == 'slot') continue
+  //     // if (match[1] == 'viewBox') continue
+  //     // if (match[1] == 'points') continue
+  //     // if (match[1] == 'label') continue
+  //     // if (match[1] == 'xmlns') continue
+  //     if (match[1].startsWith('class:')) continue
+  //     const cleanedMatch = match[3]
+  //       .replace(
+  //         /windi[`].+?[`]|(?<![-])[$](?=[{])|(?<=([{][\w\s]+[^{]*?))['"]|(?<=([{][\w\s]+[^{]*?))\s[:]|([{][\w\s]+[^{]*?[?])|^[{]|(?<=["'`)])[}]/gi,
+  //         ' '
+  //       )
+  //       .replace(/ {2,}/gi, ' ')
+  //       .replace(/["'`]/gi, '')
+  //       .replace(/\n/gi, ' ')
+
+  //     if (this.attributifies.has(match[1].toString())) {
+  //       const oldValue = this.attributifies.get(match[1].toString())
+  //       if (oldValue) {
+  //         this.attributifies.set(
+  //           match[1].toString(),
+  //           oldValue.concat(cleanedMatch.split(' '))
+  //         )
+  //       }
+  //     } else {
+  //       this.attributifies.set(match[1].toString(), cleanedMatch.split(' '))
+  //     }
+  //   }
+  //   return this
+  // }
+
+  public getStyles() {
     console.log('GET')
 
-    let iC: Set<string> = new Set([])
-    // if (this.configuration.experimental?.icons != undefined) {
-    //   iC = new Set(
-    //     this.classes.filter(
-    //       c =>
-    //         !c.startsWith(
-    //           this.configuration.experimental?.icons?.prefix || 'i-'
-    //         )
-    //     )
-    //   )
-    // } else {
-    //   iC = new Set(this.classes)
-    // }
-    iC = new Set(this.classes)
+    const styles = {
+      data: {
+        inline: {
+          utilities: new Set(),
+          icons: new Set(),
+          attributifies: new Set(),
+        },
+      },
+    }
 
-    const iD = new Set(this.directives)
-
-    const iE = new Set(this.expressions)
-
-    // let iI: Set<string> = new Set([])
-    // if (this.configuration.experimental?.icons != undefined) {
-    //   iI = new Set(
-    //     this.classes.filter(c =>
-    //       c.startsWith(this.configuration.experimental?.icons?.prefix || 'i-')
-    //     )
-    //   )
-    // }
+    styles.data.inline.utilities = new Set(this.utilities)
+    styles.data.inline.icons = new Set(this.icons)
 
     const iA = new Map()
     this.attributifies.forEach((v, k) => {
       const unique = new Set(v)
       iA.set(k, unique)
     })
-    return {
-      inlineClasses: iC,
-      inlineDirectives: iD,
-      inlineExpressions: iE,
-      inlineIcons: new Set(),
-      inlineAttributify: iA,
-    }
+
+    return styles
   }
 }
